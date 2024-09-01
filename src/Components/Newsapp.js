8a8b6dcdf0c19295c3edbd0432d2ba605b8f1161
Bibhuti Bhusan Rout <156004385+bibhutibhusanrout@users.noreply.github.com>
2024-09-01 09:40:38 +0530

    Update Newsapp.js

--- src/Components/Newsapp.js
+++ src/Components/Newsapp.js
@@ -7,7 +7,7 @@ const Newsapp = () => {
     const [search, setSearch] = useState("india");
     const [newsData, setNewsData] = useState(null)
     // const API_KEY = "87aa5f85ff5447cda209155157a13834";
-
+    console.log(API_KEY);
     const getData = async() =>{
         const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
         const jsonData = await response.json();
